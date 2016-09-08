6e38ad83700e4102c3edafb835df2d96db92b840
mattaniah <mattaniah.harp@gmail.com>
2016-09-08 18:24:25 -0400

    Update fragment.glsl
    If you want to use the proposed inTile function, with a step delineation, use something like this.

--- 08-frag-2/fragment.glsl
+++ 08-frag-2/fragment.glsl
@@ -1,42 +1,25 @@
 precision highp float;
 
-// #define CHECKSIZE vec2(16.0,16.0)
-// #define CHECKSKIP CHECKSIZE*2.0
-// #define ZERO vec2(0.0)
+#define WHITE   vec4(1.0,1.0, 1.0, 1.0)
 
-// gl-shader: Error compiling fragment shader:ERROR: 0:14: '<' : wrong operand types no operation '<' exists that takes a left-hand operand of type '2-component vector of float' and a right operand of type 'const mediump 2-component vector of float' (or there is no acceptable conversion)
+bool inTile(vec2 p, float tileSize) {
+  vec2 ptile = step(0.5, fract(0.5 * p / tileSize));
+  return ptile.x == ptile.y;
+}
 
-// gl-shader: Error compiling fragment shader:ERROR: 0:13: '*' : wrong operand types no operation '*' exists that takes a left-hand operand of type 'const mediump 2-component vector of float' and a right operand of type 'const mediump int' (or there is no acceptable conversion) ERROR: 0:14: '<' : wrong operand types no operation '<' exists that takes a left-hand operand of type '2-component vector of float' and a right operand of type 'const mediump 2-component vector of float' (or there is no acceptable conversion)
 
 void main() {
 
   //TODO: Replace this with a function that draws a checkerboard
-  vec2 point = gl_FragCoord.xy;
-  vec2 remain = mod(point, 32.0);
-  if (remain.y >= 16.0 && remain.x < 16.0) {
-    discard;
-  } else if (remain.y < 16.0 && remain.x >= 16.0) {
-    discard;
-  } else {
-    gl_FragColor = vec4(1.0,1.0,1.0,1.0);
+  vec2 coordVec = gl_FragCoord.xy;
+  
+  if (inTile(coordVec, 16.0) )
+  {
+    gl_FragColor=WHITE;
   }
-  // if (remain.x > 16.0 && remain.y > 16.0) {
-  //   gl_FragColor = vec4(1.0,1.0,1.0,1.0);
-  // } else if (remain.x <= 16.0 && remain.y < 32.0) {
-  //   gl_FragColor = vec4(1.0,1.0,1.0,1.0);
-  // } else {
-  //   discard;
+  else{
+    discard;
 
-  // }
-  // vec2 size = vec2(16.0, 16.0);
-  // point = floor(point / size);
-
-
-  // if (mod(point.x, 2.0) == 0.0 && mod(point.y, 2.0) == 0.0) {
-  //   gl_FragColor = vec4(1.0, 1.0);
-  // } else {
-  //   discard;
-  // }
-  // discard;
+  }
 
 }
